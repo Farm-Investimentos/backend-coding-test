359dd2e4217c2b1fd18696121318c3a02d91758c
Rodrigo G de Souza <rodrigo.neofx@gmail.com>
2024-02-22 22:29:31 -0300

    Melhorando a Enum

--- src/main/java/br/com/farmtech/productmanager/domain/StatusEnum.java
+++ src/main/java/br/com/farmtech/productmanager/domain/StatusEnum.java
@@ -2,14 +2,25 @@ package br.com.farmtech.productmanager.domain;
 
 public enum StatusEnum {
 
-    ATIVO,
-    INATIVO,
-    FORA_DE_LINHA,
-    EM_HOMOLOGACAO;
+    ATIVO("ativo"),
+    INATIVO("inativo"),
+    FORA_DE_LINHA("fora de linha"),
+    EM_HOMOLOGACAO("em homologação");
+
+    private String statusFriendly;
+
+    StatusEnum(String statusFriendly) {
+        this.statusFriendly = statusFriendly;
+    }
 
     public static StatusEnum fromString(String text) {
         for (StatusEnum status : StatusEnum.values()) {
             if (status.name().equalsIgnoreCase(text)) {
+                return status;
+            }
+        }
+        for (StatusEnum status : StatusEnum.values()) {
+            if (status.statusFriendly.equalsIgnoreCase(text)) {
                 return status;
             }
         }
